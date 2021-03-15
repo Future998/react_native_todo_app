b014ead70bc10823813319723421613de45ad5c1
Future998 <burmatov202002@gmail.com>
2021-03-15 21:43:30 +0300

    first_commit

---
+++ src/components/AddTodo.js
@@ -0,0 +1,46 @@
+import React, { useState } from 'react'
+import { View, StyleSheet, TextInput, Button, Alert } from 'react-native'
+import { THEME } from '../theme'
+
+export const AddTodo = ({ onSubmit }) => {
+  const [value, setValue] = useState('')
+
+  const pressHandler = () => {
+    if (value.trim()) {
+      onSubmit(value)
+      setValue('')
+    } else {
+      Alert.alert('Name of todo is NULL')
+    }
+  }
+
+  return (
+    <View style={styles.block}>
+      <TextInput
+        style={styles.input}
+        onChangeText={setValue}
+        value={value}
+        placeholder='Enter Todo'
+        autoCorrect={false}
+        autoCapitalize='none'
+      />
+      <Button title='+' onPress={pressHandler} />
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  block: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    marginBottom: 15
+  },
+  input: {
+    width: '90%',
+    padding: 10,
+    borderStyle: 'solid',
+    borderBottomWidth: 2,
+    borderBottomColor: THEME.MAIN_COLOR
+  }
+})
